6d7932935d7f286eb47a0440e865753339efbbd3
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-02 22:50:47 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datomic-component
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,32 @@
+(defproject datomic-component "1.0.0"
+  :description "Datomic Component"
+  :url "https://github.com/macielti/datomic-component"
+
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.4.9"]
+            [com.github.liquidz/antq "RELEASE"]]
+
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [hashp "0.2.2"]]
+
+  :profiles {:dev {:resource-paths ^:replace ["test/resources"]
+
+                   :test-paths     ^:replace ["test/unit" "test/integration" "test/helpers"]
+
+                   :injections     [(require 'hashp.core)]
+
+                   :aliases        {"clean-ns"     ["clojure-lsp" "clean-ns" "--dry"] ;; check if namespaces are clean
+                                    "format"       ["clojure-lsp" "format" "--dry"] ;; check if namespaces are formatted
+                                    "diagnostics"  ["clojure-lsp" "diagnostics"]
+                                    "lint"         ["do" ["clean-ns"] ["format"] ["diagnostics"]]
+                                    "clean-ns-fix" ["clojure-lsp" "clean-ns"]
+                                    "format-fix"   ["clojure-lsp" "format"]
+                                    "lint-fix"     ["do" ["clean-ns-fix"] ["format-fix"]]
+                                    "outdated"     ["with-profile" "antq" "run" "-m" "antq.core"]}
+                   :repl-options   {:init-ns common-clj.schema.core}}}
+
+  :repl-options {:init-ns datomic-component.core}
+
+  :resource-paths ["resources"])
