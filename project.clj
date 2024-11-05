287a8a3c41b86053f05b66987470561281fefd54
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-11-05 06:24:56 -0300

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,25 +1,26 @@
-(defproject net.clojars.macielti/datomic-component "1.0.0"
+(defproject net.clojars.macielti/datomic-component "1.0.1"
   :description "Datomic Component"
   :url "https://github.com/macielti/datomic-component"
 
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.4.9"]
-            [com.github.liquidz/antq "RELEASE"]]
-
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [com.taoensso/timbre "6.6.1"]
                  [diehard "0.11.12"]
-                 [hashp "0.2.2"]
-                 [prismatic/schema "1.4.1"]
-                 [nubank/matcher-combinators "3.9.1"]
-                 [com.datomic/peer "1.0.7187"]
-                 [integrant "0.11.0"]]
+                 [com.datomic/peer "1.0.7260"]
+                 [integrant "0.13.1"]]
 
   :profiles {:dev {:resource-paths ^:replace ["test/resources"]
 
                    :test-paths     ^:replace ["test/unit" "test/integration" "test/helpers"]
+
+                   :plugins        [[com.github.clojure-lsp/lein-clojure-lsp "1.4.13"]
+                                    [com.github.liquidz/antq "RELEASE"]]
+
+                   :dependencies   [[nubank/matcher-combinators "3.9.1"]
+                                    [prismatic/schema "1.4.1"]
+                                    [hashp "0.2.2"]]
 
                    :injections     [(require 'hashp.core)]
 
@@ -29,8 +30,7 @@
                                     "lint"         ["do" ["clean-ns"] ["format"] ["diagnostics"]]
                                     "clean-ns-fix" ["clojure-lsp" "clean-ns"]
                                     "format-fix"   ["clojure-lsp" "format"]
-                                    "lint-fix"     ["do" ["clean-ns-fix"] ["format-fix"]]
-                                    "outdated"     ["with-profile" "antq" "run" "-m" "antq.core"]}
+                                    "lint-fix"     ["do" ["clean-ns-fix"] ["format-fix"]]}
                    :repl-options   {:init-ns common-clj.schema.core}}}
 
   :repl-options {:init-ns datomic-component.core}
